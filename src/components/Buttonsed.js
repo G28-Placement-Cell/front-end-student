a7ab5ea409ec45d3257d4f651aa2b44106c80324
Sahil <123401404+Sahil-911@users.noreply.github.com>
2023-09-17 21:57:39 +0530

    update button and table style

--- src/components/Buttonsed.js
+++ src/components/Buttonsed.js
@@ -1,23 +1,82 @@
 import { useState } from "react";
 
-
 export const Buttoned = () => {
-    const [stads, setStads] = useState(false);
-    return (
+  const [stads, setStads] = useState(false);
+
+  return (
+    <>
+      {!stads && (
+        <button
+          style={{
+            backgroundColor: "#493D72",
+            color: "white",
+            height: -10,
+            fontSize: 15,
+            alignItems: "center",
+            justifyContent: "center",
+            transition: "background-color 0.3s ease-in-out", // Add transition here
+          }}
+          className="btn btn-lg pb-2"
+          onClick={() => {
+            setStads(!stads);
+          }}
+        >
+          REGISTER
+        </button>
+      )}
+      {stads && (
         <>
-            {!stads && <button style={{ backgroundColor: "#493D72", color: "white",height: -10, fontSize: 15, alignItems: "center", justifyContent:"center"}} className="btn btn-lg pb-2" onClick={() => { setStads(!stads) }}>REGISTER</button>}
-            {stads &&
-                <>
-                    <button style={{ backgroundColor: "#493D72", color: "white" }} className="btn btn-sm pb-2" onClick={() => { setStads(!stads) }}> DEREGISTER </button>
-                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center',justifySelf:'flex-end' , inlineSize:110}}>
-                        <div className="pb-2 d-flex flex-column gap-1 px-1 pe-1 my-1" style={{ border: '1px solid #cc0000'}}>
-                            <h5 className="me-0 pe-0">Resume</h5>
-                            <button style={{ backgroundColor: "#493D72", color: "white" }} className="btn btn-sm"> DOWNLOAD</button>
-                            <button style={{ backgroundColor: "#493D72", color: "white"}} className="btn btn-sm"> UPDATE</button>
-                        </div>
-                    </div>
-                </>
-            }
+          <button
+            style={{
+              backgroundColor: "#493D72",
+              color: "white",
+              transition: "background-color 0.3s ease-in-out", // Add transition here
+            }}
+            className="btn btn-sm pb-2"
+            onClick={() => {
+              setStads(!stads);
+            }}
+          >
+            DEREGISTER
+          </button>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              justifySelf: "flex-end",
+              inlineSize: 110,
+            }}
+          >
+            <div
+              className="pb-2 d-flex flex-column gap-1 px-1 pe-1 my-1"
+              style={{ border: "1px solid #2B2442", borderRadius:'5px' , transition: "border-color 0.3s ease-in-out" }} // Add transition here
+            >
+              <h5 className="me-0 pe-0 mb-0" style={{color:'#2B2442'}}>Resume</h5>
+              <button
+                style={{
+                  backgroundColor: "#493D72",
+                  color: "white",
+                  transition: "background-color 0.3s ease-in-out", // Add transition here
+                }}
+                className="btn btn-sm"
+              >
+                DOWNLOAD
+              </button>
+              <button
+                style={{
+                  backgroundColor: "#493D72",
+                  color: "white",
+                  transition: "background-color 0.3s ease-in-out", // Add transition here
+                }}
+                className="btn btn-sm"
+              >
+                UPDATE
+              </button>
+            </div>
+          </div>
         </>
-    );
-}
+      )}
+    </>
+  );
+};
--- src/pages/Tablet.js
+++ src/pages/Tablet.js
@@ -15,77 +15,77 @@ import Footer from '../components/Footer';
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: '#493D72',
-      color: 'white',
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 16,
-      
-    },
-  }));
-  
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    '&:last-child td, &:last-child th': {
-      border: 0,
-    },
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  }));
-  
-  function createData(name, type,cpi,link,open_for,registration_open,registration_end) {
-    return { name, type,cpi,link,open_for,registration_open,registration_end };
-  }
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#493D72',
+    color: 'white',
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 16,
+
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+}));
+
+function createData(name, type, cpi, link, open_for, registration_open, registration_end) {
+  return { name, type, cpi, link, open_for, registration_open, registration_end };
+}
 
 export const Tablet = () => {
-    const [stats,setStatus] = useState(true);
+  const [stats, setStatus] = useState(true);
 
-    const rows = [
-        createData('Micryyyyyydffffffffosoft', "Summer Internship", "NONE","https://www.microsoft.com/en-in/","btech-ict,btech-mnc", "12-20-2023 06:00", "12-20-2023 08:00"),
-        createData('Google', "Summer Internship", 7,"https://www.google.com/","btech-ict,btech-mnc", "12-20-2023 06:00", "12-20-2023 08:00"),
-        createData('Google', "Job", 7,"https://www.google.com/","btech-ict,btech-mnc", "12-20-2023 06:00", "12-20-2023 08:00"),
-    ];
+  const rows = [
+    createData('Micryyyyyydffffffffosoft', "Summer Internship", "NONE", "https://www.microsoft.com/en-in/", "btech-ict,btech-mnc", "12-20-2023 06:00", "12-20-2023 08:00"),
+    createData('Google', "Summer Internship", 7, "https://www.google.com/", "btech-ict,btech-mnc", "12-20-2023 06:00", "12-20-2023 08:00"),
+    createData('Google', "Job", 7, "https://www.google.com/", "btech-ict,btech-mnc", "12-20-2023 06:00", "12-20-2023 08:00"),
+  ];
 
-return (
-<>
-    <Header />  
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700}} aria-label="customized table">
-        <TableHead>
-          <TableRow >
-            <StyledTableCell>COMPANY NAME</StyledTableCell>
-            <StyledTableCell align="right">TYPE</StyledTableCell>
-            <StyledTableCell align="right">CPI</StyledTableCell>
-            <StyledTableCell align="right">OPEN FOR</StyledTableCell>
-            <StyledTableCell align="right">REGISTRATION STARTS</StyledTableCell>
-            <StyledTableCell align="right">REGISTRATION ENDS</StyledTableCell>
-            <StyledTableCell align="right">STATUS</StyledTableCell>
-            <StyledTableCell align="right">ACTION</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow className="mt-10 py-10" key={row.name}>
-              <StyledTableCell  component="th" scope="row">
-                <a style={{color: "red", textDecoration: "none"}} href={row.link} target="_blank">{row.name}</a>
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.type}</StyledTableCell>
-              <StyledTableCell align="right">{row.cpi}</StyledTableCell>
-              <StyledTableCell align="right">{row.open_for}</StyledTableCell>
-              <StyledTableCell align="right">{row.registration_open}</StyledTableCell>
-              <StyledTableCell align="right">{row.registration_end}</StyledTableCell>
-              <StyledTableCell align="right">{stats && "OPEN"}{!stats && "CLOSED"}</StyledTableCell>
-              <StyledTableCell align="right" style={{alignItems:'end', display:'flex', flexDirection:'column', justifyContent:'end', columnWidth: 50}}><Buttoned /></StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <Footer />
-    </>  
-);
+  return (
+    <>
+      <Header />
+      <TableContainer component={Paper} sx={{borderRadius:0}}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow >
+              <StyledTableCell>COMPANY NAME</StyledTableCell>
+              <StyledTableCell align="right">TYPE</StyledTableCell>
+              <StyledTableCell align="right">CPI</StyledTableCell>
+              <StyledTableCell align="right">OPEN FOR</StyledTableCell>
+              <StyledTableCell align="right">REGISTRATION STARTS</StyledTableCell>
+              <StyledTableCell align="right">REGISTRATION ENDS</StyledTableCell>
+              <StyledTableCell align="right">STATUS</StyledTableCell>
+              <StyledTableCell align="right">ACTION</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow className="mt-10 py-10" key={row.name}>
+                <StyledTableCell component="th" scope="row">
+                  <a style={{ color: "#591e85", textDecoration: "none" }} href={row.link} target="_blank">{row.name}</a>
+                </StyledTableCell>
+                <StyledTableCell align="right">{row.type}</StyledTableCell>
+                <StyledTableCell align="right">{row.cpi}</StyledTableCell>
+                <StyledTableCell align="right">{row.open_for}</StyledTableCell>
+                <StyledTableCell align="right">{row.registration_open}</StyledTableCell>
+                <StyledTableCell align="right">{row.registration_end}</StyledTableCell>
+                <StyledTableCell align="right">{stats && "OPEN"}{!stats && "CLOSED"}</StyledTableCell>
+                <StyledTableCell align="right" style={{ alignItems: 'end', display: 'flex', flexDirection: 'column', justifyContent: 'end', columnWidth: 50 }}><Buttoned /></StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Footer />
+    </>
+  );
 }
