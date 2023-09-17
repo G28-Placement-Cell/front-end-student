d5252a72666d462edce9b32333043235677af784
LEARNER17184 <202101497@daiict.ac.in>
2023-09-17 21:17:16 +0530

    set up of front end student

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Head } from './previous/Head';
+import { Job } from './previous/Job';
+import DataGrid from 'react-data-grid';
+import { Tablet } from './pages/Tablet';
+  
+function App() {
+  
+  return (
+    <Tablet />
+  );
+
+}
+
+export default App;
