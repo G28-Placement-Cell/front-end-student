9052d0db9a61654d3f37725715c894b28b5b936a
LEARNER17184 <202101497@daiict.ac.in>
2023-10-31 01:19:09 +0530

    Companies-page-updated,footer-updated

--- src/App.js
+++ src/App.js
@@ -22,18 +22,20 @@ function App() {
       <Router>
         <Header />
         <ToastContainer />
-        <Routes>
-          <Route path='/' element={<StudentLogin />} />
-          <Route path='/register' element={<StudentRegister />} />
-          <Route path='/Profile' element={<StudentProfile />} />
-          <Route path='/companies' element={<Tablet />} />
-          <Route path='/announcements' element={<Announcement />} />
-          <Route path='/performance' />
-          {/* <Route path='/updateresume'  /> */}
-          <Route path='/changepassword' element={<ChangePassword />} />
-          <Route path='/logout' />
-          <Route path='/*' element={<Errored />} />
-        </Routes>
+        <div style={{ minHeight: "84vh" }}>
+          <Routes>
+            <Route path='/' element={<StudentLogin />} />
+            <Route path='/register' element={<StudentRegister />} />
+            <Route path='/Profile' element={<StudentProfile />} />
+            <Route path='/companies' element={<Tablet />} />
+            <Route path='/announcements' element={<Announcement />} />
+            <Route path='/performance' />
+            {/* <Route path='/updateresume'  /> */}
+            <Route path='/changepassword' element={<ChangePassword />} />
+            <Route path='/logout' />
+            <Route path='/*' element={<Errored />} />
+          </Routes>
+        </div>
         <Footer />
       </Router>
     </>
---
+++ src/components/ChechDate.js
@@ -0,0 +1,19 @@
+import moment from 'moment';
+import { useState, useEffect } from 'react';
+
+export const CheckDate = ({ reg_open, reg_end }) => {
+    var currentDate = new Date();
+    const [stats, setStatus] = useState(false);
+    const date1 = new Date(reg_open);
+    const date2 = new Date(reg_end);
+    useEffect(() => {
+        if (currentDate.getTime() > date1.getTime() && currentDate.getTime() < date2.getTime())
+            setStatus(!stats);
+        console.log(date1, date2);
+    }, []);
+    return (
+        <>
+            {stats ? "OPEN" : "CLOSED"}
+        </>
+    );
+}
