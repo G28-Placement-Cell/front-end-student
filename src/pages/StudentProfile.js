5637f20bed7252d0234d2fe4cf47cd29748e2e05
ShivangKacha <shivangkachasbk9@gmail.com>
2023-10-30 22:01:36 +0530

    data fetched to student profile

--- src/pages/StudentProfile.js
+++ src/pages/StudentProfile.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 
 function StudentProfile() {
   const [student, setStudent] = useState({});//student object
+  const [loading, setLoading] = useState(true);//loading state
 
   // const [getdata, { isLoading }] = useGetdataMutation();
 
@@ -31,7 +32,7 @@ function StudentProfile() {
 
   useEffect(() => {
     console.log(localStorage.getItem('token'));
-    fetch('http://localhost:5000/api/student/profile', {
+    fetch('http://localhost:8000/api/student/profile', {
       method: 'GET',
       headers: {
         'content-type': 'application/json',
@@ -40,8 +41,10 @@ function StudentProfile() {
     }).then((res) => res.json()).then((data) => {
       console.log(data);
       setStudent(data.stu);
+      setLoading(false);
     }).catch((err) => {
       console.log(err);
+      setLoading(false);
     });
   }, []);
 
@@ -60,10 +63,10 @@ function StudentProfile() {
     whiteSpace: 'nowrap',
     width: 1,
   });
+  if (loading) return (<div>Loading...</div>);
   return (
-    < div className="container" >
+    <div className="container" >
       <div className="main-body">
-
         <div className="row gutters-sm">
           <div className="col-md-4 mb-3">
             <div className="card">
@@ -71,8 +74,8 @@ function StudentProfile() {
                 <div className="d-flex flex-column align-items-center text-center">
                   <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" className="rounded-circle" width={150} />
                   <div className="mt-3">
-                    <h4>student name</h4>
-                    <p id="student id" className="text-secondary mb-1">student id</p>
+                    <h4 id='student_name'>{student?.name}</h4>
+                    <p id="student id" className="text-secondary mb-1">{student?.student_id}</p>
                     <p id="verify" className="text-muted font-size-sm">Your profile is APPROVED </p>
                   </div>
                 </div>
@@ -82,43 +85,43 @@ function StudentProfile() {
               <ul className="list-group list-group-flush">
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">Email</h6>
-                  <span id="Email" className="text-secondary">0000@daiict.ac.in</span>
+                  <span id="Email" className="text-secondary">{student?.email.main}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">10th Percentage</h6>
-                  <span id="10thPercentage" className="text-secondary">00.00%</span>
+                  <span id="10thPercentage" className="text-secondary">{student?.results.tenth_percentage}%</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">12th Percentage</h6>
-                  <span id="12thPercentage" className="text-secondary">00.00%</span>
+                  <span id="12thPercentage" className="text-secondary">{student?.results.twelve_percentage}%</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">CPI</h6>
-                  <span id="CPI" className="text-secondary">0.00</span>
+                  <span id="CPI" className="text-secondary">{student?.cpi}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">Current BackLogs</h6>
-                  <span id="CurrentBackLogs" className="text-secondary">0</span>
+                  <span id="CurrentBackLogs" className="text-secondary">{student?.academics.current_backlogs}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">Total BackLogs</h6>
-                  <span id="TotalBackLogs" className="text-secondary">0</span>
+                  <span id="TotalBackLogs" className="text-secondary">{student?.academics.total_backlogs}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">Branch</h6>
-                  <span id="Branch" className="text-secondary">0</span>
+                  <span id="Branch" className="text-secondary">{student?.academics.branch}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">Domain</h6>
-                  <span id="Domain" className="text-secondary">IT</span>
+                  <span id="Domain" className="text-secondary">{student?.domain}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">Registering For</h6>
-                  <span id="RegisteringFor" className="text-secondary">-</span>
+                  <span id="RegisteringFor" className="text-secondary">{student?.registering_for}</span>
                 </li>
                 <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                   <h6 className="mb-0">Interested in Placement</h6>
-                  <span id="InterestedinPlacement" className="text-secondary">Yes/NO</span>
+                  <span id="InterestedinPlacement" className="text-secondary">Yes</span>
                 </li>
                 {/* <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                     <h6 className="mb-0">Out of Placement Drive</h6>
@@ -135,7 +138,7 @@ function StudentProfile() {
                     <h6 className="mb-0">First Name</h6>
                   </div>
                   <div id="FirstName" className="col-sm-9 text-secondary">
-                    -
+                    {student?.name}
                   </div>
                 </div>
                 <hr />
@@ -144,7 +147,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Last Name</h6>
                   </div>
                   <div id="LastName" className="col-sm-9 text-secondary">
-                    -
+                    {student?.surname}
                   </div>
                 </div>
                 <hr />
@@ -153,7 +156,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Alternate Email Address</h6>
                   </div>
                   <div id="AlternateEmailAddress" className="col-sm-9 text-secondary">
-                    xyz@gmail.com
+                    {student?.email.alt}
                   </div>
                 </div>
                 <hr />
@@ -162,7 +165,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Skype ID</h6>
                   </div>
                   <div id="SkypeID" className="col-sm-9 text-secondary">
-                    123:jdgajdg:123
+                    {student?.skype_id}
                   </div>
                 </div>
                 <hr />
@@ -171,7 +174,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Date Of Birth</h6>
                   </div>
                   <div id="DOB" className="col-sm-9 text-secondary">
-                    00-00-0000
+                    {student?.dob}
                   </div>
                 </div>
                 <hr />
@@ -180,7 +183,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Gender</h6>
                   </div>
                   <div id="Gender" className="col-sm-9 text-secondary">
-                    -
+                    {student?.gender}
                   </div>
                 </div>
                 <hr />
@@ -189,7 +192,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Mobile Number</h6>
                   </div>
                   <div id="MobileNumber" className="col-sm-9 text-secondary">
-                    1234567890
+                    {student?.phone_number.main}
                   </div>
                 </div>
                 <hr />
@@ -198,7 +201,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Alternate Mobile Number</h6>
                   </div>
                   <div id="AlternateMobileNumber" className="col-sm-9 text-secondary">
-                    1234567890
+                    {student?.phone_number.alt}
                   </div>
                 </div>
                 <hr />
@@ -207,7 +210,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Father Name</h6>
                   </div>
                   <div id="FatherName" className="col-sm-9 text-secondary">
-                    bhai
+                    {student?.father_name}
                   </div>
                 </div>
                 <hr />
@@ -217,7 +220,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Mother Name</h6>
                   </div>
                   <div id="MotherName" className="col-sm-9 text-secondary">
-                    ben
+                    {student?.mother_name}
                   </div>
                 </div>
                 <hr />
@@ -227,7 +230,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Permanent Address</h6>
                   </div>
                   <div id="PermanentAddress" className="col-sm-9 text-secondary">
-                    -
+                    {student?.address.per_address}
                   </div>
                 </div>
                 <hr />
@@ -237,7 +240,7 @@ function StudentProfile() {
                     <h6 className="mb-0">Current Address</h6>
                   </div>
                   <div id="CurrentAddress" className="col-sm-9 text-secondary">
-                    -
+                    {student?.address.cur_address}
                   </div>
                 </div>
                 <hr />
