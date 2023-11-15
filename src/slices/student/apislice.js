170bd11f9340e1cfb0ccc1c737e486befec16175
ShivangKacha <shivangkachasbk9@gmail.com>
2023-11-15 15:03:23 +0530

    backend link changed

--- src/slices/student/apislice.js
+++ src/slices/student/apislice.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({ baseUrl: 'https://back-end-production-ee2f.up.railway.app/' });
 
+
 export const apislice = createApi({
     baseQuery,
     tagTypes: ['Student'],
