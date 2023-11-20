29495cf42620a23714499bd4cfe57e94b11b91e6
LEARNER17184 <202101497@daiict.ac.in>
2023-11-21 02:14:35 +0530

    Navbar-updated-different-for-verified-notverified

---
+++ src/Sidebared/SidebarNot.js
@@ -0,0 +1,69 @@
+import React from 'react';
+// import * as IoIcons from 'react-icons/io';
+import * as BiIcons from 'react-icons/bi';
+import * as TfiIcons from "react-icons/tfi";
+import * as HiIcons from "react-icons/hi"
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+
+// import * as RxIcons from "react-icons/rx"
+import * as TbIcons from 'react-icons/tb'
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../slices/student/authslice';
+import { useNavigate } from 'react-router-dom';
+import { useLogoutMutation } from '../slices/student/studentApislice';
+
+
+
+// const dispatch = useDispatch();
+// const [logoutapicall] = useLogoutMutation();
+
+// const navigate = useNavigate();
+// const logoutHandler = async () => {
+//   try {
+//     await logoutapicall().unwrap();
+//     dispatch(logout());
+//     navigate('/');
+//   }
+//   catch (err) {
+//     console.log(err);
+//   }
+// }
+export const SidebarDatanot = [
+
+  {
+    title: 'Profile',
+    path: '/profile',
+    icon: <BiIcons.BiUser />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Update Resume',
+    path: '/updateresume',
+    icon: <BiIcons.BiUpload />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Update Profile pic',
+    path: '/updateProfilepic',
+    icon: <BiIcons.BiUpload />,
+    cName: 'nav-text'
+  },
+  // {
+  //   title: 'Update Resume',
+  //   path: '/updateresume',
+  //   icon: <RxIcons.RxUpdate />,
+  //   cName: 'nav-text'
+  // },
+  {
+    title: 'Change Password',
+    path: '/changepassword',
+    icon: <TbIcons.TbArrowsExchange />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Logout',
+    path: '/logout', // or whatever path you prefer
+    icon: <LogoutRoundedIcon />,
+    cName: 'nav-text',
+  },
+];
